02f38def9b83330e8aeaf269f47fb42ec88d486d
Miguel Torres <miguelfreelancer56577@gmail.com>
2020-12-18 23:21:10 -0600

    AWS and Profile configuration.
    Added implementation to use aws s3 services to store images.
    Added profiles to work using either local or wsd services.
    Removed all hardcode on application.
    Cleaned up code to make it more readable.

--- src/main/java/com/github/mangelt/lab1/util/ApiConstants.java
+++ src/main/java/com/github/mangelt/lab1/util/ApiConstants.java
@@ -15,6 +15,9 @@ public class ApiConstants {
 //	request params
 	public static final String REQ_PARAM_IMAGE_FILE = "imageFile";
 	public static final String REQ_PARAM_IMAGE_NAME = "imageName";
+	public static final String REQ_PARAM_IMAGE_FORMAT = "format";
+	public static final String REQ_PARAM_IMAGE_SIZE = "size";
+	public static final String REQ_PARAM_UPLOADED_DATE = "uploaded_date";
 	
 //	signs and cords used along with the application 
 	public static final String SIGN_COMMA = ",";
@@ -26,6 +29,13 @@ public class ApiConstants {
 	public static final String EXP_VALIDATION_FIELDS = "Please verify the correctness of required fields.";
 	public static final String EXP_ERROR_READ_FILE = "There was an error to get content's file.";
 	public static final String EXP_ERROR_READ_AVAILABLE_IMAGES = "There was an error to list available images from storage.";
+	public static final String EXP_ERROR_READ_METADATA_IMAGES = "There was an error getting metadata from the image.";
+	public static final String EXP_ERROR_NOT_EXIST_BUCKET = "The bucket your trying to access does not exists.";
+	
+//	message format
+	public static final String MSG_FORMAT_ADDING_INFO = ": {}";
+	public static final String MSG_FORMAT_IMAGE_PATH = " IMAGE PATH {}.";
+	
 	
 //	success messages 
 	public static final String MSG_OK_IMAGE_SAVE = "The image was stored successfully.";
@@ -33,6 +43,19 @@ public class ApiConstants {
 	
 //	configuration values 
 	public static final Long RULE_MAX_IMAGE_SIZE = 1000000L;
+	
+//	spring profiles
+	public static final String PROFILE_AWS = "aws";
+	public static final String PROFILE_LOCAL = "local";
+	
+//	bean names
+	public static final String BEAN_S3_CLIENT = "s3_client";
+	
+//	spring properties
+	public static final String APP_CONFIG_IMAGE_DIRECTORY = "${app.config.image.directory}";
+	public static final String APP_CONFIG_IMAGE_AWS_BUCKET_NAME = "${app.config.image.aws.bucket-name}";
+	public static final String APP_CONFIG_IMAGE_AWS_ACCESSKEY = "${app.config.image.aws.accesskey}";
+	public static final String APP_CONFIG_IMAGE_AWS_SECRETKEY = "${app.config.image.aws.secretkey}";
 	
 	
 }
