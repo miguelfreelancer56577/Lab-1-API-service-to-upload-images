977e7bc83f969c3d412534395dc6afdf13d387cf
MIguel A. Torres <miguelfreelancer56577@gmail.com>
2020-12-19 09:22:23 -0600

    Revert "Feature/implementation for s3 service (#4)" (#5)
    This reverts commit d6e6c146d0ad415e96d0e1809afdb6500a6d020d.

--- src/main/java/com/github/mangelt/lab1/component/ImageValidator.java
+++
@@ -1,7 +0,0 @@
-package com.github.mangelt.lab1.component;
-
-import com.github.mangelt.lab1.domain.ImageDetailsPayload;
-
-public interface ImageValidator {
-	boolean isValid(ImageDetailsPayload image);
-}
