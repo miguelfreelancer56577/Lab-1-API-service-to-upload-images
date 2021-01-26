6aac06f5d6b7a9ecc81ea5e9bf2b95c9ae914583
Miguel Torres <miguelfreelancer56577@gmail.com>
2021-01-26 17:32:59 -0600

    Added azure configuration and log info for bean providers.

--- src/main/java/com/github/mangelt/lab1/config/AwsS3Config.java
+++ src/main/java/com/github/mangelt/lab1/config/AwsS3Config.java
@@ -13,8 +13,11 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.github.mangelt.lab1.util.ApiConstants;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Configuration
 @Profile(ApiConstants.PROFILE_AWS)
+@Slf4j
 public class AwsS3Config {
 	
 	@Value(ApiConstants.APP_CONFIG_IMAGE_AWS_ACCESSKEY)
@@ -26,11 +29,13 @@ public class AwsS3Config {
 	
 	@Bean
 	AWSCredentials awsCredencials() {
+		log.info("Setting up aws credencials.");
 		return new BasicAWSCredentials(accesskey, secretkey);
 	}
 	
 	@Bean(name = ApiConstants.BEAN_S3_CLIENT)
 	AmazonS3 s3Client(AWSCredentials credentials) {
+		log.info("Setting up aws client.");
 		return AmazonS3ClientBuilder
 				  .standard()
 				  .withCredentials(new AWSStaticCredentialsProvider(credentials))
